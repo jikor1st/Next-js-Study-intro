91a13cb8b1ffc8d72a6d8a92455ea23ce30c26c6
jikor1st <jikor1st@naver.com>
2022-04-10 19:58:36 +0900

    [study] ssr study
    ssr 서버사이드렌더링까지 기초 공부 완료

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   reactStrictMode: true,
-}
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/old-blog/:path*",
+  //       destination: "/new-blog/:path*",
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies/popular",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`,
+      },
+    ];
+  },
+};
